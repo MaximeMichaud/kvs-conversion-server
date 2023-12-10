a3b03f6405011ca885528345ecf75a91b8863aac
Maxime Michaud <14856114+MaximeMichaud@users.noreply.github.com>
2023-12-09 22:53:33 -0500

    USER_HOME based on FTP_USER environment variable

--- create_folders.sh
+++ create_folders.sh
@@ -1,14 +1,15 @@
 #!/bin/sh
 
-# Read the environment variable
+# Read the environment variables
 NUM_FOLDERS=${NUM_FOLDERS:-10} # Default value of 10 if not defined
+FTP_USER=${FTP_USER:-user} # Default FTP user if not defined
 
-# Base location for user "user" folders
-USER_HOME="/home/vsftpd/user"
+# Base location for FTP user's folders
+USER_HOME="/home/vsftpd/${FTP_USER}"
 
 # Create folders and set up cron jobs
 for i in $(seq 1 $NUM_FOLDERS); do
-  # Create the folder in the "user" user's directory
+  # Create the folder in the FTP user's directory
   DIR_PATH="$USER_HOME/$i"
   mkdir -p "$DIR_PATH"
 
