f054bf1d47b4d343e313cfa36777ff03fc1ded1c
Maxime Michaud <14856114+MaximeMichaud@users.noreply.github.com>
2023-12-10 04:03:01 -0500

    fix: Double quote to prevent globbing and word splitting

--- create_folders.sh
+++ create_folders.sh
@@ -9,9 +9,9 @@ PHP_VERSION=${PHP_VERSION:-php7.4} # Default to php7.4, can be set to php8.1
 USER_HOME="/home/vsftpd/${FTP_USER}"
 
 # Create folders
-for i in $(seq -w 1 $NUM_FOLDERS); do
+for i in $(seq -w 1 "$NUM_FOLDERS"); do
   # Create the folder in the FTP user's directory
-  DIR_PATH="$USER_HOME/$(printf "%02d" $i)"
+  DIR_PATH="$USER_HOME/$(printf "%02d" "$i")"
   mkdir -p "$DIR_PATH"
 done
 
@@ -19,9 +19,9 @@ done
 CRON_FILE=$(mktemp)
 crontab -l > "$CRON_FILE" 2>/dev/null
 echo "#KVS Folders Cron Jobs" >> "$CRON_FILE"
-for i in $(seq 1 $NUM_FOLDERS); do
+for i in $(seq 1 "$NUM_FOLDERS"); do
   # Format folder number with leading zero
-  FOLDER_NUM=$(printf "%02d" $i)
+  FOLDER_NUM=$(printf "%02d" "$i")
   # Use the original number for log file name
   LOG_NUM=$i
   echo "* * * * * [ -f $USER_HOME/$FOLDER_NUM/remote_cron.php ] && /usr/bin/$PHP_VERSION $USER_HOME/$FOLDER_NUM/remote_cron.php >> /var/log/cron$LOG_NUM.log 2>&1" >> "$CRON_FILE"
