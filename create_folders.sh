f13d6470e19e0b85340f26dc655a45e44d899c75
Maxime Michaud <14856114+MaximeMichaud@users.noreply.github.com>
2024-04-14 19:38:41 -0400

    Ensure directory numbering starts from 01 and apply consistent zero-padding

--- create_folders.sh
+++ create_folders.sh
@@ -8,9 +8,8 @@ PHP_VERSION=${PHP_VERSION:-php7.4} # Default to php7.4, can be set to php8.1
 # Base location for FTP user's folders
 USER_HOME="/home/vsftpd/${FTP_USER}"
 
-# Create folders
-for i in $(seq -w 1 "$NUM_FOLDERS"); do
-  # Create the folder in the FTP user's directory
+# Create folders with leading zero in folder names if the number is less than 10
+for i in $(seq 1 "$NUM_FOLDERS"); do
   DIR_PATH="$USER_HOME/$(printf "%02d" "$i")"
   mkdir -p "$DIR_PATH"
 done
@@ -20,11 +19,8 @@ CRON_FILE=$(mktemp)
 crontab -l > "$CRON_FILE" 2>/dev/null
 echo "#KVS Folders Cron Jobs" >> "$CRON_FILE"
 for i in $(seq 1 "$NUM_FOLDERS"); do
-  # Format folder number with leading zero
   FOLDER_NUM=$(printf "%02d" "$i")
-  # Use the original number for log file name
-  LOG_NUM=$i
-  echo "* * * * * [ -f $USER_HOME/$FOLDER_NUM/remote_cron.php ] && /usr/bin/$PHP_VERSION $USER_HOME/$FOLDER_NUM/remote_cron.php >> /var/log/cron$LOG_NUM.log 2>&1" >> "$CRON_FILE"
+  echo "* * * * * [ -f $USER_HOME/$FOLDER_NUM/remote_cron.php ] && /usr/bin/$PHP_VERSION $USER_HOME/$FOLDER_NUM/remote_cron.php >> /var/log/cron${FOLDER_NUM}.log 2>&1" >> "$CRON_FILE"
 done
 crontab "$CRON_FILE"
 rm "$CRON_FILE"
